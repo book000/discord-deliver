9eabaf16d3a8b58df15e356fedf6cd38956ded6d
Tomachi [ICHIGO] <tomachi@tomacheese.com>
2022-07-04 16:06:44 +0900

    fix: expose & example

--- example/src/entrypoint.sh
+++ example/src/entrypoint.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
 
-curl -X POST -H "Content-Type: application/json" -d '{"content":"Hello, world!"}' "http://discord-deliver/${DISCORD_CHANNEL_ID}"
+if ping discord-deliver -c 1 -W 10; then
+    curl -X POST -H "Content-Type: application/json" -d '{"content":"Hello, world!"}' "http://discord-deliver/${DISCORD_CHANNEL_ID}"
+fi
